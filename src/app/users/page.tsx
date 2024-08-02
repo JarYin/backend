0f13a2313dd1de7d1ee122c7505ab45e616de2e7
JarYin <tempest353@hotmail.com>
2024-08-02 10:39:00 +0700

    update fetch

--- src/app/users/page.tsx
+++ src/app/users/page.tsx
@@ -7,7 +7,7 @@ export default function Page() {
   useEffect(() => {
     async function getUsers() {
       try {
-        const res = await fetch('http://localhost:3000/api/users');
+        const res = await fetch('https://backend-b5e7q05eh-jaryins-projects.vercel.app/api/users');
         if (!res.ok) {
           console.error('Failed to fetch data');
           return;
